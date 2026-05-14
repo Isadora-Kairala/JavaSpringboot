bc460aa0232e9a246b25ca0530141668c500948b
Isadora-Kairala <isadorarequer7191@gmail.com>
2026-05-14 20:41:12 -0300

    PostgreSQL 14/05

---
+++ bd/src/main/java/dc/unifacef/bd/model/Produto.java
@@ -0,0 +1,28 @@
+package dc.unifacef.bd.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="produtos")
+
+public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String nome;
+
+    @Column (nullable = false)
+    private Double preco;
+
+    @Column (length = 500)
+    private String descricao;
+
+
+
+}
+
